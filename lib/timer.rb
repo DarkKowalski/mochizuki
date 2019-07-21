a16723ebd9dd3885e5bc9c5de2e04eb27b9d7d0a
kowalski <darkkowalski2012@gmail.com>
2019-07-21 17:37:58 +0800

    implement: basic features

---
+++ lib/timer.rb
@@ -0,0 +1,23 @@
+require 'telegram/bot'
+require_relative './fetcher'
+
+def timer
+  fetcher = Fetcher.new($post_data)
+  begin
+    fetcher.fetch
+    dorm = fetcher.get_dorm
+    balance = fetcher.get_balance
+  rescue 
+    dorm = -1
+    balance = -1
+  end
+
+  if dorm.to_i > 0 && balance.to_i < $threshold.to_i
+    $low_power = true
+    $timer_enable = false
+  else
+    $low_power = false
+  end
+
+  return balance
+end
